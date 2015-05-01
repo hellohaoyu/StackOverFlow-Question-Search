c0ef68a7338f911e0a3ddc9930fea2caf85b6765
Haoyu-Mac <hc6as@virginia.edu>
2015-05-01 17:18:23 -0400

    Searching works

--- controllers/utils.py
+++ controllers/utils.py
@@ -1,23 +1,18 @@
 from urllib2 import *
-import simplejson
-
-
-
+# import json as simplejson
+from django.utils import simplejson
 
 def getSearchJSON(url):
 	searchData = urlopen(url)
-    searchJSON = simplejson.load(searchData)
-
-    return searchJSON['response']['docs']
+	searchJSON = simplejson.load(searchData)
+	return searchJSON['response']['docs']
 
 def getSearchURL(query):
-	return "http://localhost:8983/solr/test/select?q=find+min+in+a+list+java&wt=json&indent=true&fl=*+score&rows=10"
+	return "http://localhost:8983/solr/test/select?q=find+min+in+a+list+java&wt=json&fl=Title%2C+Id%2C+Tags&indent=true&rows=10"
 
-def searchData(dateItems, query)
-	# data = []
-	# l = len(dateItems)
+# input questions, dateitems and query are used to create query url
+def searchData(question = None, dateItems = None, query = None):
 	url = getSearchURL(query)
 	jsonRs = getSearchJSON(url)
-
 	return jsonRs
 
